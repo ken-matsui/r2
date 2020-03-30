88c326b151044edbf65504f09977db0c9e992be2
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 22:48:57 -0700

    Apply tslint to src/QuoteAggregator.ts

--- src/QuoteAggregator.ts
+++ src/QuoteAggregator.ts
@@ -1,11 +1,11 @@
-﻿import { injectable, inject } from 'inversify';
-import { IConfigStore, BrokerConfig, QuoteSide, Broker, IQuote } from './types';
-import { getLogger } from '@bitr/logger';
-import * as _ from 'lodash';
-import symbols from './symbols';
-import BrokerAdapterRouter from './BrokerAdapterRouter';
-import { DateTime, Interval } from 'luxon';
-import { AwaitableEventEmitter } from '@bitr/awaitable-event-emitter';
+﻿import { AwaitableEventEmitter } from "@bitr/awaitable-event-emitter";
+import { getLogger } from "@bitr/logger";
+import { inject, injectable } from "inversify";
+import * as _ from "lodash";
+import { DateTime, Interval } from "luxon";
+import BrokerAdapterRouter from "./BrokerAdapterRouter";
+import symbols from "./symbols";
+import { Broker, BrokerConfig, IConfigStore, IQuote, QuoteSide } from "./types";
 
 @injectable()
 export default class QuoteAggregator extends AwaitableEventEmitter {
@@ -16,42 +16,42 @@ export default class QuoteAggregator extends AwaitableEventEmitter {
 
   constructor(
     @inject(symbols.ConfigStore) private readonly configStore: IConfigStore,
-    private readonly brokerAdapterRouter: BrokerAdapterRouter
+    private readonly brokerAdapterRouter: BrokerAdapterRouter,
   ) {
     super();
   }
 
-  async start(): Promise<void> {
-    this.log.debug('Starting Quote Aggregator...');
+  public async start(): Promise<void> {
+    this.log.debug("Starting Quote Aggregator...");
     const { iterationInterval } = this.configStore.config;
     this.timer = setInterval(this.aggregate.bind(this), iterationInterval);
     this.log.debug(`Iteration interval is set to ${iterationInterval}`);
     await this.aggregate();
-    this.log.debug('Started Quote Aggregator.');
+    this.log.debug("Started Quote Aggregator.");
   }
 
-  async stop(): Promise<void> {
-    this.log.debug('Stopping Quote Aggregator...');
+  public async stop(): Promise<void> {
+    this.log.debug("Stopping Quote Aggregator...");
     if (this.timer) {
       clearInterval(this.timer);
     }
-    this.log.debug('Stopped Quote Aggregator.');
+    this.log.debug("Stopped Quote Aggregator.");
   }
 
   private async aggregate(): Promise<void> {
     if (this.isRunning) {
-      this.log.debug('Aggregator is already running. Skipped iteration.');
+      this.log.debug("Aggregator is already running. Skipped iteration.");
       return;
     }
     try {
       this.isRunning = true;
-      this.log.debug('Aggregating quotes...');
+      this.log.debug("Aggregating quotes...");
       const enabledBrokers = this.getEnabledBrokers();
-      const fetchTasks = enabledBrokers.map(x => this.brokerAdapterRouter.fetchQuotes(x));
+      const fetchTasks = enabledBrokers.map((x) => this.brokerAdapterRouter.fetchQuotes(x));
       const quotesMap = await Promise.all(fetchTasks);
       const allQuotes = _.flatten(quotesMap);
       await this.setQuotes(this.fold(allQuotes, this.configStore.config.priceMergeSize));
-      this.log.debug('Aggregated.');
+      this.log.debug("Aggregated.");
     } catch (ex) {
       this.log.error(ex.message);
       this.log.debug(ex.stack);
@@ -62,17 +62,17 @@ export default class QuoteAggregator extends AwaitableEventEmitter {
 
   private async setQuotes(value: IQuote[]): Promise<void> {
     this.quotes = value;
-    this.log.debug('New quotes have been set.');
-    this.log.debug('Calling onQuoteUpdated...');
-    await this.emitParallel('quoteUpdated', this.quotes);
-    this.log.debug('onQuoteUpdated done.');
+    this.log.debug("New quotes have been set.");
+    this.log.debug("Calling onQuoteUpdated...");
+    await this.emitParallel("quoteUpdated", this.quotes);
+    this.log.debug("onQuoteUpdated done.");
   }
 
   private getEnabledBrokers(): Broker[] {
     return _(this.configStore.config.brokers)
-      .filter(b => b.enabled)
-      .filter(b => this.timeFilter(b))
-      .map(b => b.broker)
+      .filter((b) => b.enabled)
+      .filter((b) => this.timeFilter(b))
+      .map((b) => b.broker)
       .value();
   }
 
@@ -81,10 +81,10 @@ export default class QuoteAggregator extends AwaitableEventEmitter {
       return true;
     }
     const current = DateTime.local();
-    const outOfPeriod = period => {
+    const outOfPeriod = (period) => {
       const interval = Interval.fromISO(`${period[0]}/${period[1]}`);
       if (!interval.isValid) {
-        this.log.warn('Invalid noTradePeriods. Ignoring the config.');
+        this.log.warn("Invalid noTradePeriods. Ignoring the config.");
         return true;
       }
       return !interval.contains(current);
@@ -96,13 +96,13 @@ export default class QuoteAggregator extends AwaitableEventEmitter {
     return _(quotes)
       .groupBy((q: IQuote) => {
         const price = q.side === QuoteSide.Ask ? _.ceil(q.price / step) * step : _.floor(q.price / step) * step;
-        return _.join([price, q.broker, QuoteSide[q.side]], '#');
+        return _.join([price, q.broker, QuoteSide[q.side]], "#");
       })
       .map((value: IQuote[], key) => ({
         broker: value[0].broker,
+        price: Number(key.substring(0, key.indexOf("#"))),
         side: value[0].side,
-        price: Number(key.substring(0, key.indexOf('#'))),
-        volume: _.sumBy(value, q => q.volume)
+        volume: _.sumBy(value, (q) => q.volume),
       }))
       .value();
   }
