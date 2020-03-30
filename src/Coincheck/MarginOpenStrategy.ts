80ff7b8cc8a0849790546a1497cfd49b964821ef
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-30 00:44:56 -0700

    Apply tslint to src/Coincheck/MarginOpenStrategy.ts

--- src/Coincheck/MarginOpenStrategy.ts
+++ src/Coincheck/MarginOpenStrategy.ts
@@ -1,25 +1,25 @@
-import { CashMarginTypeStrategy } from './types';
-import BrokerApi from './BrokerApi';
-import { IOrder, OrderStatus, OrderSide, CashMarginType } from '../types';
-import { eRound } from '../util';
-import * as _ from 'lodash';
+import * as _ from "lodash";
+import { CashMarginType, IOrder, OrderSide, OrderStatus } from "../types";
+import { eRound } from "../util";
+import BrokerApi from "./BrokerApi";
+import { CashMarginTypeStrategy } from "./types";
 
 export default class MarginOpenStrategy implements CashMarginTypeStrategy {
   constructor(private readonly brokerApi: BrokerApi) {}
 
-  async send(order: IOrder): Promise<void> {
+  public async send(order: IOrder): Promise<void> {
     if (order.cashMarginType !== CashMarginType.MarginOpen) {
       throw new Error();
     }
     const request = {
-      pair: 'btc_jpy',
+      pair: "btc_jpy",
       order_type: this.getBrokerOrderType(order),
       amount: order.size,
-      rate: order.price
+      rate: order.price,
     };
     const reply = await this.brokerApi.newOrder(request);
     if (!reply.success) {
-      throw new Error('Send failed.');
+      throw new Error("Send failed.");
     }
     order.sentTime = reply.created_at;
     order.status = OrderStatus.New;
@@ -27,19 +27,19 @@ export default class MarginOpenStrategy implements CashMarginTypeStrategy {
     order.lastUpdated = new Date();
   }
 
-  async getBtcPosition(): Promise<number> {
+  public async getBtcPosition(): Promise<number> {
     const positions = await this.brokerApi.getAllOpenLeveragePositions();
-    const longPosition = _.sumBy(positions.filter(p => p.side === 'buy'), p => p.amount);
-    const shortPosition = _.sumBy(positions.filter(p => p.side === 'sell'), p => p.amount);
+    const longPosition = _.sumBy(positions.filter((p) => p.side === "buy"), (p) => p.amount);
+    const shortPosition = _.sumBy(positions.filter((p) => p.side === "sell"), (p) => p.amount);
     return eRound(longPosition - shortPosition);
   }
 
   private getBrokerOrderType(order: IOrder): string {
     switch (order.side) {
       case OrderSide.Buy:
-        return 'leverage_buy';
+        return "leverage_buy";
       case OrderSide.Sell:
-        return 'leverage_sell';
+        return "leverage_sell";
       default:
         throw new Error();
     }
