21edbf61810bc8ec0812a9827f21cca2b10ea2b8
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 23:09:13 -0700

    Apply tslint to src/Quoine/CashStrategy.ts

--- src/Quoine/CashStrategy.ts
+++ src/Quoine/CashStrategy.ts
@@ -1,14 +1,14 @@
-import { CashMarginTypeStrategy } from './types';
-import BrokerApi from './BrokerApi';
+import BrokerApi from "./BrokerApi";
+import { CashMarginTypeStrategy } from "./types";
 
 export default class CashStrategy implements CashMarginTypeStrategy {
   constructor(private readonly brokerApi: BrokerApi) {}
 
-  async getBtcPosition(): Promise<number> {
+  public async getBtcPosition(): Promise<number> {
     const accounts = await this.brokerApi.getAccountBalance();
-    const account = accounts.find(b => b.currency === 'BTC');
+    const account = accounts.find((b) => b.currency === "BTC");
     if (account === undefined) {
-      throw new Error ('Unable to find the account.');
+      throw new Error ("Unable to find the account.");
     }
     return account.balance;
   }
