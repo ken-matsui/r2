0f71549e9f52d592f63447d1dd05ab9fe2849a06
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 23:07:42 -0700

    Apply tslint to src/Quoine/NetOutStrategy.ts

--- src/Quoine/NetOutStrategy.ts
+++ src/Quoine/NetOutStrategy.ts
@@ -1,15 +1,15 @@
-import { CashMarginTypeStrategy } from './types';
-import BrokerApi from './BrokerApi';
-import * as _ from 'lodash';
+import * as _ from "lodash";
+import BrokerApi from "./BrokerApi";
+import { CashMarginTypeStrategy } from "./types";
 
 export default class NetOutStrategy implements CashMarginTypeStrategy {
   constructor(private readonly brokerApi: BrokerApi) {}
 
-  async getBtcPosition(): Promise<number> {
+  public async getBtcPosition(): Promise<number> {
     const accounts = await this.brokerApi.getTradingAccounts();
-    const account = _.find(accounts, b => b.currency_pair_code === 'BTCJPY');
+    const account = _.find(accounts, (b) => b.currency_pair_code === "BTCJPY");
     if (!account) {
-      throw new Error('Unable to find the account.');
+      throw new Error("Unable to find the account.");
     }
     return account.position;
   }
