3468ebd6f854f31e52f855833fa576efd4e66863
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-30 00:38:01 -0700

    Apply tslint to src/HistoricalOrderStore.ts

--- src/HistoricalOrderStore.ts
+++ src/HistoricalOrderStore.ts
@@ -1,36 +1,36 @@
-import { IHistoricalOrderStore, IOrder } from './types';
-import { reviveOrder } from './OrderImpl';
-import { ChronoDB, TimeSeries } from '@bitr/chronodb';
-import { EventEmitter } from 'events';
+import { ChronoDB, TimeSeries } from "@bitr/chronodb";
+import { EventEmitter } from "events";
+import { reviveOrder } from "./OrderImpl";
+import { IHistoricalOrderStore, IOrder } from "./types";
 
 class EmittableHistoricalOrderStore extends EventEmitter implements IHistoricalOrderStore {
   private readonly timeSeries: TimeSeries<IOrder>;
 
   constructor(chronoDB: ChronoDB) {
     super();
-    this.timeSeries = chronoDB.getTimeSeries<IOrder>('HistoricalOrder', order => reviveOrder(order));
+    this.timeSeries = chronoDB.getTimeSeries<IOrder>("HistoricalOrder", (order) => reviveOrder(order));
   }
 
-  get(key: string): Promise<IOrder> {
+  public get(key: string): Promise<IOrder> {
     return this.timeSeries.get(key);
   }
 
-  getAll(): Promise<{ key: string; value: IOrder }[]> {
+  public getAll(): Promise<Array<{ key: string; value: IOrder }>> {
     return this.timeSeries.getAll();
   }
 
-  put(value: IOrder): Promise<string> {
-    this.emit('change');
+  public put(value: IOrder): Promise<string> {
+    this.emit("change");
     return this.timeSeries.put(value);
   }
 
-  del(key: string): Promise<void> {
-    this.emit('change');
+  public del(key: string): Promise<void> {
+    this.emit("change");
     return this.timeSeries.del(key);
   }
 
-  delAll(): Promise<{}> {
-    this.emit('change');
+  public delAll(): Promise<{}> {
+    this.emit("change");
     return this.timeSeries.delAll();
   }
 }
