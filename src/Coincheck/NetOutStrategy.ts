68053ccdbdb0bbdd1008e4a6d7cdb14f057899ca
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-30 00:42:25 -0700

    Apply tslint to src/Coincheck/NetOutStrategy.ts

--- src/Coincheck/NetOutStrategy.ts
+++ src/Coincheck/NetOutStrategy.ts
@@ -1,20 +1,20 @@
-import { CashMarginTypeStrategy, NewOrderRequest, LeveragePosition } from './types';
-import BrokerApi from './BrokerApi';
-import { IOrder, OrderStatus, OrderSide, CashMarginType, OrderType } from '../types';
-import { eRound, almostEqual } from '../util';
-import * as _ from 'lodash';
+import * as _ from "lodash";
+import { CashMarginType, IOrder, OrderSide, OrderStatus, OrderType } from "../types";
+import { almostEqual, eRound } from "../util";
+import BrokerApi from "./BrokerApi";
+import { CashMarginTypeStrategy, LeveragePosition, NewOrderRequest } from "./types";
 
 export default class NetOutStrategy implements CashMarginTypeStrategy {
   constructor(private readonly brokerApi: BrokerApi) {}
 
-  async send(order: IOrder): Promise<void> {
+  public async send(order: IOrder): Promise<void> {
     if (order.cashMarginType !== CashMarginType.NetOut) {
       throw new Error();
     }
     const request = await this.getNetOutRequest(order);
     const reply = await this.brokerApi.newOrder(request);
     if (!reply.success) {
-      throw new Error('Send failed.');
+      throw new Error("Send failed.");
     }
     order.sentTime = reply.created_at;
     order.status = OrderStatus.New;
@@ -22,39 +22,39 @@ export default class NetOutStrategy implements CashMarginTypeStrategy {
     order.lastUpdated = new Date();
   }
 
-  async getBtcPosition(): Promise<number> {
+  public async getBtcPosition(): Promise<number> {
     const positions = await this.brokerApi.getAllOpenLeveragePositions();
-    const longPosition = _.sumBy(positions.filter(p => p.side === 'buy'), p => p.amount);
-    const shortPosition = _.sumBy(positions.filter(p => p.side === 'sell'), p => p.amount);
+    const longPosition = _.sumBy(positions.filter((p) => p.side === "buy"), (p) => p.amount);
+    const shortPosition = _.sumBy(positions.filter((p) => p.side === "sell"), (p) => p.amount);
     return eRound(longPosition - shortPosition);
   }
 
   private async getNetOutRequest(order: IOrder): Promise<NewOrderRequest> {
     const openPositions = await this.brokerApi.getAllOpenLeveragePositions();
-    const targetSide = order.side === OrderSide.Buy ? 'sell' : 'buy';
+    const targetSide = order.side === OrderSide.Buy ? "sell" : "buy";
     const candidates = _(openPositions)
-      .filter(p => p.side === targetSide)
-      .filter(p => almostEqual(p.amount, order.size, 1))
+      .filter((p) => p.side === targetSide)
+      .filter((p) => almostEqual(p.amount, order.size, 1))
       .value();
-    if (order.symbol !== 'BTC/JPY') {
-      throw new Error('Not supported');
+    if (order.symbol !== "BTC/JPY") {
+      throw new Error("Not supported");
     }
-    const pair = 'btc_jpy';
+    const pair = "btc_jpy";
     const rate = order.type === OrderType.Market ? undefined : order.price;
     const request = { pair, rate };
     if (candidates.length === 0) {
       return {
         ...request,
-        order_type: order.side === OrderSide.Buy ? 'leverage_buy' : 'leverage_sell',
-        amount: order.size
+        order_type: order.side === OrderSide.Buy ? "leverage_buy" : "leverage_sell",
+        amount: order.size,
       };
     }
     const targetPosition = _.last(candidates) as LeveragePosition;
     return {
       ...request,
-      order_type: order.side === OrderSide.Buy ? 'close_short' : 'close_long',
+      order_type: order.side === OrderSide.Buy ? "close_short" : "close_long",
       amount: targetPosition.amount,
-      position_id: Number(targetPosition.id)
+      position_id: Number(targetPosition.id),
     };
   }
 }
