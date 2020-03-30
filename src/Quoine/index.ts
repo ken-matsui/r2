bbda7a308abd8dea8026b5eff91e6624fe4012f7
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 23:05:33 -0700

    Apply tslint to src/Quoine/index.ts

--- src/Quoine/index.ts
+++ src/Quoine/index.ts
@@ -1,5 +1,5 @@
-import BrokerAdapterImpl from './BrokerAdapterImpl';
-import { IBrokerConfigType } from '../types';
+import { IBrokerConfigType } from "../types";
+import BrokerAdapterImpl from "./BrokerAdapterImpl";
 
 export function create(config: IBrokerConfigType) {
   return new BrokerAdapterImpl(config);
