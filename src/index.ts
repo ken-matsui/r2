946c50aa11f86499138b7b670d75ec956263c2d4
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 22:21:01 -0700

    Apply tslint to src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import AppRoot from './AppRoot';
-import { exec } from 'child_process';
-import container from './container.config';
+import { exec } from "child_process";
+import AppRoot from "./AppRoot";
+import container from "./container.config";
 
-process.title = 'r2app';
+process.title = "r2app";
 const app = new AppRoot(container);
 app.start();
 
@@ -11,16 +11,16 @@ function exit(code: number = 0) {
   process.exit(code);
 }
 
-process.on('SIGINT', async () => {
-  console.log('SIGINT received. Stopping...');
+process.on("SIGINT", async () => {
+  console.log("SIGINT received. Stopping...");
   await app.stop();
-  console.log('Stopped app.');
+  console.log("Stopped app.");
   exit();
 });
 
-process.on('unhandledRejection', async (reason, p) => {
+process.on("unhandledRejection", async (reason, p) => {
   console.error(reason);
   await app.stop();
-  console.log('Stopped app.');
+  console.log("Stopped app.");
   exit(1);
 });
