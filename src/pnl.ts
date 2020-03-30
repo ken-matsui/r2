8f1008c4696683cf9d2b9d0bea82cf4a98ed3098
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 22:13:05 -0700

    Apply tslint to src/pnl.ts

--- src/pnl.ts
+++ src/pnl.ts
@@ -1,17 +1,18 @@
-import OrderImpl from './OrderImpl';
-import * as _ from 'lodash';
-import { findBrokerConfig } from './configUtil';
-import { OrderSide, ConfigRoot } from './types';
+import * as _ from "lodash";
+import { findBrokerConfig } from "./configUtil";
+import OrderImpl from "./OrderImpl";
+import { ConfigRoot, OrderSide } from "./types";
 
 export function calcCommission(price: number, volume: number, commissionPercent: number): number {
   return commissionPercent !== undefined ? price * volume * (commissionPercent / 100) : 0;
 }
 
 export function calcProfit(orders: OrderImpl[], config: ConfigRoot): { profit: number; commission: number } {
-  const commission = _(orders).sumBy(o => {
+  const commission = _(orders).sumBy((o) => {
     const brokerConfig = findBrokerConfig(config, o.broker);
     return calcCommission(o.averageFilledPrice, o.filledSize, brokerConfig.commissionPercent);
   });
-  const profit = _(orders).sumBy(o => (o.side === OrderSide.Sell ? 1 : -1) * o.filledNotional) - commission;
+  const profit = _(orders).sumBy((o) =>
+      (o.side === OrderSide.Sell ? 1 : -1) * o.filledNotional) - commission;
   return { profit, commission };
 }
