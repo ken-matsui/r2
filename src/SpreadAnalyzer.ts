fe040c40c97f1016d37bef460647d6fc5ea6ccbe
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 22:40:44 -0700

    Apply tslint to src/SpreadAnalyzer.ts

--- src/SpreadAnalyzer.ts
+++ src/SpreadAnalyzer.ts
@@ -1,24 +1,24 @@
-﻿import { injectable, inject } from 'inversify';
+﻿import { getLogger } from "@bitr/logger";
+import Decimal from "decimal.js";
+import { inject, injectable } from "inversify";
+import * as _ from "lodash";
+import { findBrokerConfig } from "./configUtil";
+import { LOT_MIN_DECIMAL_PLACE } from "./constants";
+import t from "./intl";
+import OrderImpl from "./OrderImpl";
+import { calcCommission } from "./pnl";
+import symbols from "./symbols";
 import {
-  IConfigStore,
-  QuoteSide,
-  ISpreadAnalysisResult,
   IBrokerMap,
-  OrderSide,
-  IQuote,
   IBrokerPosition,
+  IConfigStore,
+  IQuote,
+  ISpreadAnalysisResult,
+  ISpreadStat,
   OrderPair,
-  ISpreadStat
-} from './types';
-import { getLogger } from '@bitr/logger';
-import * as _ from 'lodash';
-import t from './intl';
-import symbols from './symbols';
-import Decimal from 'decimal.js';
-import { findBrokerConfig } from './configUtil';
-import { LOT_MIN_DECIMAL_PLACE } from './constants';
-import OrderImpl from './OrderImpl';
-import { calcCommission } from './pnl';
+  OrderSide,
+  QuoteSide,
+} from "./types";
 
 @injectable()
 export default class SpreadAnalyzer {
@@ -26,27 +26,27 @@ export default class SpreadAnalyzer {
 
   constructor(@inject(symbols.ConfigStore) private readonly configStore: IConfigStore) {}
 
-  async analyze(
+  public async analyze(
     quotes: IQuote[],
     positionMap: IBrokerMap<IBrokerPosition>,
-    closingPair?: OrderPair
+    closingPair?: OrderPair,
   ): Promise<ISpreadAnalysisResult> {
     if (closingPair && closingPair[0].size !== closingPair[1].size) {
-      throw new Error('Invalid closing pair.');
+      throw new Error("Invalid closing pair.");
     }
 
     const { config } = this.configStore;
     if (_.isEmpty(positionMap)) {
-      throw new Error('Position map is empty.');
+      throw new Error("Position map is empty.");
     }
     let filteredQuotes = _(quotes)
-      .filter(q => this.isAllowedByCurrentPosition(q, positionMap[q.broker]))
-      .filter(q => new Decimal(q.volume).gte(
+      .filter((q) => this.isAllowedByCurrentPosition(q, positionMap[q.broker]))
+      .filter((q) => new Decimal(q.volume).gte(
         (closingPair ? closingPair[0].size : config.minSize) *
           _.floor(config.maxTargetVolumePercent !== undefined
             ? 100 / config.maxTargetVolumePercent
             : 1)))
-      .orderBy(['price'])
+      .orderBy(["price"])
       .value();
     if (closingPair) {
       const isOppositeSide = (o: OrderImpl, q: IQuote) =>
@@ -54,11 +54,11 @@ export default class SpreadAnalyzer {
       const isSameBroker = (o: OrderImpl, q: IQuote) => o.broker === q.broker;
       filteredQuotes = _(filteredQuotes)
         .filter(
-          q =>
+          (q) =>
             (isSameBroker(closingPair[0], q) && isOppositeSide(closingPair[0], q)) ||
-            (isSameBroker(closingPair[1], q) && isOppositeSide(closingPair[1], q))
+            (isSameBroker(closingPair[1], q) && isOppositeSide(closingPair[1], q)),
         )
-        .filter(q => new Decimal(q.volume).gte(closingPair[0].size))
+        .filter((q) => new Decimal(q.volume).gte(closingPair[0].size))
         .value();
     }
     const { ask, bid } = this.getBest(filteredQuotes);
@@ -82,51 +82,51 @@ export default class SpreadAnalyzer {
     const midNotional = _.mean([ask.price, bid.price]) * targetVolume;
     const profitPercentAgainstNotional = _.round(targetProfit / midNotional * 100, LOT_MIN_DECIMAL_PLACE);
     const spreadAnalysisResult = {
-      bid,
       ask,
-      invertedSpread,
       availableVolume,
-      targetVolume,
+      bid,
+      invertedSpread,
+      profitPercentAgainstNotional,
       targetProfit,
-      profitPercentAgainstNotional
+      targetVolume,
     };
     this.log.debug(`Analysis done. Result: ${JSON.stringify(spreadAnalysisResult)}`);
     return spreadAnalysisResult;
   }
 
-  async getSpreadStat(quotes: IQuote[]): Promise<ISpreadStat | undefined> {
+  public async getSpreadStat(quotes: IQuote[]): Promise<ISpreadStat | undefined> {
     const { config } = this.configStore;
     const filteredQuotes = _(quotes)
-      .filter(q => new Decimal(q.volume).gte(config.minSize))
-      .orderBy(['price'])
+      .filter((q) => new Decimal(q.volume).gte(config.minSize))
+      .orderBy(["price"])
       .value();
-    const asks = _(filteredQuotes).filter(q => q.side === QuoteSide.Ask);
-    const bids = _(filteredQuotes).filter(q => q.side === QuoteSide.Bid);
+    const asks = _(filteredQuotes).filter((q) => q.side === QuoteSide.Ask);
+    const bids = _(filteredQuotes).filter((q) => q.side === QuoteSide.Bid);
     if (asks.isEmpty() || bids.isEmpty()) {
       return undefined;
     }
     const byBroker = _(filteredQuotes)
-      .groupBy(q => q.broker)
-      .mapValues(qs => {
+      .groupBy((q) => q.broker)
+      .mapValues((qs) => {
         const { ask, bid } = this.getBest(qs);
         const spread = ask && bid ? ask.price - bid.price : undefined;
         return { ask, bid, spread };
       })
       .value();
     const flattened = _(byBroker)
-      .map((v, k) => [v.ask, v.bid])  
-      .flatten()      
-      .filter(q => q !== undefined)
+      .map((v, k) => [v.ask, v.bid])
+      .flatten()
+      .filter((q) => q !== undefined)
       .value() as IQuote[];
     const { ask: bestAsk, bid: bestBid } = this.getBest(flattened) as { ask: IQuote, bid: IQuote };
     const { ask: worstAsk, bid: worstBid } = this.getWorst(flattened) as { ask: IQuote, bid: IQuote };
     const bestCase = this.getEstimate(bestAsk, bestBid);
     const worstCase = this.getEstimate(worstAsk, worstBid);
     return {
-      timestamp: Date.now(),
-      byBroker,
       bestCase,
-      worstCase
+      byBroker,
+      timestamp: Date.now(),
+      worstCase,
     };
   }
 
@@ -141,39 +141,39 @@ export default class SpreadAnalyzer {
     const profitPercentAgainstNotional = _.round(targetProfit / midNotional * 100, LOT_MIN_DECIMAL_PLACE);
     return {
       ask,
+      availableVolume,
       bid,
       invertedSpread,
-      availableVolume,
-      targetVolume,
+      profitPercentAgainstNotional,
       targetProfit,
-      profitPercentAgainstNotional
+      targetVolume,
     };
   }
 
   private getBest(quotes: IQuote[]) {
-    const ordered = _.orderBy(quotes, ['price']);
+    const ordered = _.orderBy(quotes, ["price"]);
     const ask = _(ordered)
-      .filter(q => q.side === QuoteSide.Ask)
+      .filter((q) => q.side === QuoteSide.Ask)
       .first();
     const bid = _(ordered)
-      .filter(q => q.side === QuoteSide.Bid)
+      .filter((q) => q.side === QuoteSide.Bid)
       .last();
     return { ask, bid };
   }
 
   private getWorst(quotes: IQuote[]) {
-    const ordered = _.orderBy(quotes, ['price']);
+    const ordered = _.orderBy(quotes, ["price"]);
     const ask = _(ordered)
-      .filter(q => q.side === QuoteSide.Ask)
+      .filter((q) => q.side === QuoteSide.Ask)
       .last();
     const bid = _(ordered)
-      .filter(q => q.side === QuoteSide.Bid)
+      .filter((q) => q.side === QuoteSide.Bid)
       .first();
     return { ask, bid };
   }
 
   private calculateTotalCommission(quotes: IQuote[], targetVolume: number): number {
-    return _(quotes).sumBy(q => {
+    return _(quotes).sumBy((q) => {
       const brokerConfig = findBrokerConfig(this.configStore.config, q.broker);
       return calcCommission(q.price, targetVolume, brokerConfig.commissionPercent);
     });
