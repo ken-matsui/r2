752cfb452cf6c720eca307df0a2e95d557b9d8bc
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 22:37:20 -0700

    Apply tslint to src/SpreadStatTimeSeries.ts

--- src/SpreadStatTimeSeries.ts
+++ src/SpreadStatTimeSeries.ts
@@ -1,36 +1,36 @@
-import { ISpreadStat } from './types';
-import { ChronoDB, TimeSeries } from '@bitr/chronodb';
-import { eRound } from './util';
-import { EOL } from 'os';
+import { ChronoDB, TimeSeries } from "@bitr/chronodb";
+import { EOL } from "os";
+import { ISpreadStat } from "./types";
+import { eRound } from "./util";
 
 export const getSpreadStatTimeSeries = (chronoDB: ChronoDB): TimeSeries<ISpreadStat> =>
-  chronoDB.getTimeSeries<ISpreadStat>('SpreadStat');
+  chronoDB.getTimeSeries<ISpreadStat>("SpreadStat");
 
 export const spreadStatCsvHeader = [
-  'timestamp',
-  '[best] ask broker',
-  '[best] ask price',
-  '[best] ask volume',
-  '[best] bid broker',
-  '[best] bid price',
-  '[best] bid volume',
-  '[best] spread',
-  '[best] available volume',
-  '[best] target volume',
-  '[best] target profit',
-  '[best] target profit percent',
-  '[worst] ask broker',
-  '[worst] ask price',
-  '[worst] ask volume',
-  '[worst] bid broker',
-  '[worst] bid price',
-  '[worst] bid volume',
-  '[worst] spread',
-  '[worst] available volume',
-  '[worst] target volume',
-  '[worst] target profit',
-  '[worst] target profit percent'
-].join(', ') + EOL;
+  "timestamp",
+  "[best] ask broker",
+  "[best] ask price",
+  "[best] ask volume",
+  "[best] bid broker",
+  "[best] bid price",
+  "[best] bid volume",
+  "[best] spread",
+  "[best] available volume",
+  "[best] target volume",
+  "[best] target profit",
+  "[best] target profit percent",
+  "[worst] ask broker",
+  "[worst] ask price",
+  "[worst] ask volume",
+  "[worst] bid broker",
+  "[worst] bid price",
+  "[worst] bid volume",
+  "[worst] spread",
+  "[worst] available volume",
+  "[worst] target volume",
+  "[worst] target profit",
+  "[worst] target profit percent",
+].join(", ") + EOL;
 
 export function spreadStatToCsv(spreadStat: ISpreadStat): string {
   return [
@@ -57,5 +57,5 @@ export function spreadStatToCsv(spreadStat: ISpreadStat): string {
     spreadStat.worstCase.targetVolume,
     spreadStat.worstCase.targetProfit,
     spreadStat.worstCase.profitPercentAgainstNotional,
-  ].join(', ') + EOL;
+  ].join(", ") + EOL;
 }
