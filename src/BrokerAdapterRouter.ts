5f83d5bdcfd141934722258d0281acd775aab930
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-30 00:57:41 -0700

    Apply tslint to src/BrokerAdapterRouter.ts

--- src/BrokerAdapterRouter.ts
+++ src/BrokerAdapterRouter.ts
@@ -1,11 +1,11 @@
-﻿import { Broker, IBrokerAdapter, IBrokerMap, IOrder, IQuote, IConfigStore } from './types';
-import { getLogger } from '@bitr/logger';
-import * as _ from 'lodash';
-import { injectable, multiInject, inject } from 'inversify';
-import symbols from './symbols';
-import BrokerStabilityTracker from './BrokerStabilityTracker';
-import OrderService from './OrderService';
-import OrderImpl from './OrderImpl';
+﻿import { getLogger } from "@bitr/logger";
+import { inject, injectable, multiInject } from "inversify";
+import * as _ from "lodash";
+import BrokerStabilityTracker from "./BrokerStabilityTracker";
+import OrderImpl from "./OrderImpl";
+import OrderService from "./OrderService";
+import symbols from "./symbols";
+import { Broker, IBrokerAdapter, IBrokerMap, IConfigStore, IOrder, IQuote } from "./types";
 
 @injectable()
 export default class BrokerAdapterRouter {
@@ -16,12 +16,12 @@ export default class BrokerAdapterRouter {
     @multiInject(symbols.BrokerAdapter) brokerAdapters: IBrokerAdapter[],
     private readonly brokerStabilityTracker: BrokerStabilityTracker,
     @inject(symbols.ConfigStore) private readonly configStore: IConfigStore,
-    private readonly orderService: OrderService
+    private readonly orderService: OrderService,
   ) {
-    this.brokerAdapterMap = _.keyBy(brokerAdapters, x => x.broker);
+    this.brokerAdapterMap = _.keyBy(brokerAdapters, (x) => x.broker);
   }
 
-  async send(order: IOrder): Promise<void> {
+  public async send(order: IOrder): Promise<void> {
     this.log.debug(order.toString());
     try {
       await this.brokerAdapterMap[order.broker].send(order);
@@ -32,36 +32,36 @@ export default class BrokerAdapterRouter {
     }
   }
 
-  async cancel(order: IOrder): Promise<void> {
+  public async cancel(order: IOrder): Promise<void> {
     this.log.debug(order.toString());
     await this.brokerAdapterMap[order.broker].cancel(order);
     this.orderService.emitOrderUpdated(order as OrderImpl);
   }
 
-  async refresh(order: IOrder): Promise<void> {
+  public async refresh(order: IOrder): Promise<void> {
     this.log.debug(order.toString());
     await this.brokerAdapterMap[order.broker].refresh(order);
     this.orderService.emitOrderUpdated(order as OrderImpl);
   }
 
-  async getPositions(broker: Broker): Promise<Map<string, number>> {
+  public async getPositions(broker: Broker): Promise<Map<string, number>> {
     try {
       // for backword compatibility, use getBtcPosition if getPositions is not defined
-      if (!_.isFunction(this.brokerAdapterMap[broker].getPositions) && this.configStore.config.symbol === 'BTC/JPY') {
+      if (!_.isFunction(this.brokerAdapterMap[broker].getPositions) && this.configStore.config.symbol === "BTC/JPY") {
         const btcPosition = await this.brokerAdapterMap[broker].getBtcPosition();
-        return new Map<string, number>([['BTC', btcPosition]]);
+        return new Map<string, number>([["BTC", btcPosition]]);
       }
       if (this.brokerAdapterMap[broker].getPositions !== undefined) {
         return await (this.brokerAdapterMap[broker].getPositions as () => Promise<Map<string, number>>)();
       }
-      throw new Error('Unable to find a method to get positions.');
+      throw new Error("Unable to find a method to get positions.");
     } catch (ex) {
       this.brokerStabilityTracker.decrement(broker);
       throw ex;
     }
   }
 
-  async fetchQuotes(broker: Broker): Promise<IQuote[]> {
+  public async fetchQuotes(broker: Broker): Promise<IQuote[]> {
     try {
       return await this.brokerAdapterMap[broker].fetchQuotes();
     } catch (ex) {
