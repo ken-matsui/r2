e50ca4185b826d5b1007caa27b63946a25dac99f
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 17:10:13 -0700

    Apply tslint to src/transport/LineIntegration.ts

--- src/transport/LineIntegration.ts
+++ src/transport/LineIntegration.ts
@@ -1,43 +1,41 @@
-import fetch, { RequestInit as FetchRequestInit } from 'node-fetch';
-import { LineConfig } from '../types';
-import * as querystring from 'querystring';
+import fetch, { RequestInit as FetchRequestInit } from "node-fetch";
+import * as querystring from "querystring";
+import { LineConfig } from "../types";
 
 export default class LineIntegration {
-  static fetchTimeout = 5000;
-  static apiUrl = 'https://notify-api.line.me/api/notify';
+  private static fetchTimeout = 5000;
+  private static apiUrl = "https://notify-api.line.me/api/notify";
 
   constructor(private readonly config: LineConfig) {
     this.config = config;
   }
 
-  handler(message: string): void {
+  public handler(message: string): void {
     const keywords = this.config.keywords;
     if (!(keywords instanceof Array)) {
       return;
     }
-    if (!keywords.some(x => message.includes(x))) {
+    if (!keywords.some((x) => message.includes(x))) {
       return;
     }
-    const payload = {
-      message
-    };
+    const payload = { message };
     const body = querystring.stringify(payload);
     const init: FetchRequestInit = {
       body,
-      method: 'POST',
       headers: {
-        Authorization: `Bearer ${this.config.token}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': body.length.toString()
+        "Authorization": `Bearer ${this.config.token}`,
+        "Content-Length": body.length.toString(),
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      timeout: LineIntegration.fetchTimeout
+      method: "POST",
+      timeout: LineIntegration.fetchTimeout,
     };
     fetch(LineIntegration.apiUrl, init)
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
-          res.text().then(s => console.log(`LINE notify failed. ${res.statusText}: ${s}`));
+          res.text().then((s) => console.log(`LINE notify failed. ${res.statusText}: ${s}`));
         }
       })
-      .catch(ex => console.log(`LINE notify failed. ${ex}`));
+      .catch((ex) => console.log(`LINE notify failed. ${ex}`));
   }
 }
