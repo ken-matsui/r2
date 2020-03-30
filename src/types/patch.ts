fedcaaaf6b1fa42d9fa005dfbc4afe58bd4a9a2c
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-03-29 20:43:22 -0700

    Apply tslint to src/types/patch.ts

--- src/types/patch.ts
+++ src/types/patch.ts
@@ -1,7 +1,7 @@
-import { Socket } from 'zeromq';
+import { Socket } from "zeromq";
 
-// patch for @types/zeromq
-export interface ZmqSocket extends Socket {
+// patch for @types/zeromq FIXME: Unused interface
+export interface IZmqSocket extends Socket {
   removeAllListeners: any;
   once: any;
 }
