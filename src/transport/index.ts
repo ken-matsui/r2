f494e3bda22a4f690218f0bd607a0f06175ab757
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-05-01 23:14:25 -0700

    Improve src/transport/index.ts

--- src/transport/index.ts
+++ src/transport/index.ts
@@ -116,7 +116,7 @@ function broadcast(clients: WebSocket[], type: string, body: any) {
 }
 
 function addIntegration(
-  Integration: { new (config: any): SlackIntegration | LineIntegration },
+  Integration: new (config: any) => SlackIntegration | LineIntegration,
   config: SlackConfig | LineConfig | undefined,
 ): void {
   if (config && config.enabled) {
